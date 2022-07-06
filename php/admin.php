48193d33e727d9f153702b551f07bbf3f54e6d63
fakihfavian24 <fakihfavianwibowo@gmail.com>
2022-07-06 19:35:34 +0700

    Menambahkan Halaman Admin dan Koneksi ke DB

---
+++ php/admin.php
@@ -0,0 +1,47 @@
+<?php
+require 'functions.php';
+$menu  = query("SELECT * FROM menu");
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Halaman Admin</title>
+</head>
+
+<body>
+
+  <h1>Daftar Menu</h1>
+
+  <table border="1" cellpadding="10" cellspacing="0">
+
+    <tr>
+      <th>No.</th>
+      <th>Gambar</th>
+      <th>Nama</th>
+      <th>Harga</th>
+    </tr>
+
+    <?php $i = 1; ?>
+    <?php foreach ($menu as $mn) : ?>
+      <tr>
+        <td><?= $i; ?></td>
+        <td><img src="../images/<?= $mn["gambar"] ?>" width="80"></td>
+        <td><?= $mn["nama"] ?></td>
+        <td><?= $mn["harga"] ?></td>
+        <td>
+          <a href="">ubah</a>
+          <a href="">hapus</a>
+        </td>
+      </tr>
+      <?php $i++; ?>
+    <?php endforeach; ?>
+
+  </table>
+
+</body>
+
+</html>
